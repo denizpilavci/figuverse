8796abd12b7143777ca90699aa501660de046204
denizpilavci <denizpilavci@gmail.com>
2026-05-23 01:58:04 +0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+let server;
 async function start() {
   await migrate();
-  app.listen(PORT, () => {
+  server = app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}...`);
   });
 }
